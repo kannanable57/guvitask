d2e28daf276b6883e276180e2e5b7a933ee061f1
SRc <SRc@hp-PC>
2024-01-17 17:42:19 +0530

    length of two question

--- src/example/length.java
+++ src/example/length.java
@@ -4,9 +4,10 @@ public class length {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String s = "Welcome to Guvi";
-System.out.println("string length is:"+s.length());//13 is the length of java point
-
+String s1 = "Welcome to Guvi";
+String s2 = "Guvi Geek";
+System.out.println("string length is:"+s1.length());//13 is the length of java point
+System.out.println("string length is:"+s2.length());//8 is the length of java point 
 	}
 
 }
