4c8f6ebba305e55b92e0c4b1173244dbefa91f85
SRc <SRc@hp-PC>
2024-01-17 14:47:22 +0530

    loop through A to Z character

---
+++ src/example/main.java
@@ -0,0 +1,13 @@
+package example;
+
+ class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char c;
+for(c = 'A'; c <= 'Z'; ++c)
+	System.out.print(c + "");
+	}
+}
+
+
