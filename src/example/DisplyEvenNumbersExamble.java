2345317e439144f5cc08f2ed65ab56eb10ffa35e
SRc <SRc@hp-PC>
2024-01-17 13:55:29 +0530

    find out even number

---
+++ src/example/DisplyEvenNumbersExamble.java
@@ -0,0 +1,27 @@
+package example;
+
+public class DisplyEvenNumbersExamble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number=100;
+System.out.print("List of even numbersfrom 1 to"+number+";");
+for(int i=1; i<=number;i++)
+{
+	//logic to check if the number is even or not
+	//if i%2 is equal to zero, the number is even
+	if(i%2==0)
+	{
+System.out.print(i+"");
+	}
+}
+	}
+}
+
+	
+
+	
+
+	
+
+
