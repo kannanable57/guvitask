04141d70a6ade2896c1b4d4e68f236cf9c9a703d
SRc <SRc@hp-PC>
2024-01-17 18:42:47 +0530

    seniorcitizen

---
+++ src/example/seniorcitizen.java
@@ -0,0 +1,26 @@
+package example;
+import java.util.Scanner;
+public class seniorcitizen {
+
+	public static void main(String[] args) { 
+	int age;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the age of the person");
+	age=sc.nextInt();
+	if(age>=60)
+	{
+	System.out.println("The person is senior citizen");
+	}
+	else
+	{
+	System.out.println("The person is not senior citizen");
+	}
+	}
+}
+	
+		
+	
+
+	
+
+
