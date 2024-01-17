b24558d07f37994fdcafd3eabf19a4c5fb9429ab
SRc <SRc@hp-PC>
2024-01-17 17:31:56 +0530

    length string

---
+++ src/example/length.java
@@ -0,0 +1,12 @@
+package example;
+
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s = "Welcome to Guvi";
+System.out.println("string length is:"+s.length());//13 is the length of java point
+
+	}
+
+}
