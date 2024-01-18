49ce4e7605e272c89ab30144c1ae49fd2cda8909
SRc <SRc@hp-PC>
2024-01-18 16:36:56 +0530

    reverse string

---
+++ src/example/Reversestring.java
@@ -0,0 +1,37 @@
+package example;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		String str = "Like";
+		String rstr = " ";
+		char ch;
+		
+		for(int i=0; i<str.length();i++) {
+			ch = str.charAt(i);
+				rstr = ch + rstr;
+		}
+		System.out.println("Reverse string = " + rstr);
+	}
+}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			
+			
+		
+
+	
+
+
