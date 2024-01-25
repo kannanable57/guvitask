0f2a6d03b1b8c1ba95c81b2aec82f1f4c88627f0
SRc <SRc@hp-PC>
2024-01-25 08:43:45 +0530

    character

---
+++ src/example/Character.java
@@ -0,0 +1,13 @@
+package example;
+
+ class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char c;
+for(c = 'A'; c <='z'; ++c);
+System.out.print(c+"");
+	}
+}
+
+
